fbd0996561098f971b9c43a93d439d8b9173394a
june.kim <junho85@gmail.com>
2020-07-07 00:17:31 +0900

    21.Null-safety

---
+++ src/main/java/kr/pe/junho85/demo/AppRunner.java
@@ -0,0 +1,15 @@
+package kr.pe.junho85.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class AppRunner implements ApplicationRunner {
+    @Autowired
+    EventService eventService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String message = eventService.createEvent(null);
+    }
+}
---
+++ src/main/java/kr/pe/junho85/demo/EventService.java
@@ -0,0 +1,11 @@
+package kr.pe.junho85.demo;
+
+import org.springframework.lang.NonNull;
+
+public class EventService {
+    @NonNull
+    public String createEvent(@NonNull String name) {
+//        return "hello " + name;
+        return null;
+    }
+}
